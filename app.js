ba3d7765ae9b8f46ce981ce1bf94e8ca644980fe
CJ B <cj@DESKTOP-56GPSTK>
2023-06-05 13:34:51 -0500

    initializing code

---
+++ app.js
@@ -0,0 +1,67 @@
+// array.map(function(x){
+//     return x * 2
+// })
+
+// const add = function(x,y){
+//     return x + y
+// }
+
+// const add = (x,y) =>{
+//     return x+y
+// }
+
+// [2,3,6,78,99,104,23].reduce(function(max,current){
+//     return Math.max(max,current)
+// });
+
+// [2,3,6,78,99,104,23].reduce((max,current)=>{
+//     return Math.max(max,current)
+// });
+
+// [1,2,3,4,5].forEach(n=>{
+//     console.log(n* 10)
+// })
+
+// const greet = () => {
+// console.log('hello')
+// }
+
+// [1,2,3,4,5,6].filter((num)=> num % 2 ===0)
+
+// const double = (n) => n * 2
+
+// [1,2,3,4,5,6,7,8].map((n)=> {
+//     if(n % 2 === 0){
+//         return 'even'
+//     }
+//     return 'odd'
+// })
+
+// [1,2,3,4,5,6,7,8].map((n)=> (n % 2 ===0 ? 'even' : 'odd'))
+
+
+// const dailyRainTotals = [[1.2,0.35,2.2],[1.7, 0.6, 0.1], [2.5,0.9,1.5]]
+
+// dailyRainTotals.map((hourRainTotals) => hourRainTotals.reduce((sum,inchesOfRain)=> sum + inchesOfRain))
+
+
+// const makeMath = (num) => 
+//     ({
+//         square : num * num, 
+//         double : num * 2
+//     })
+
+
+// const cat = {
+//     name : 'Colby',
+//     meow: function() {
+//         return `${this.name} says MEOW`
+//     }
+// }
+
+// const boy = {
+//     name: 'CJ',
+//     mrow: function () {
+//         return `${this.name} says MROW`
+//     }
+// }
